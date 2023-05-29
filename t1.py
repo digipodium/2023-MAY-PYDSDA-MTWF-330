99ded879a78ef7e2c7ad61606f095ed8a2d7ba33
zaid <xaidmetamorphos@gmail.com>
2023-05-29 17:05:07 +0530

    i/o

---
+++ t1.py
@@ -0,0 +1,17 @@
+from turtle import * # import all functions from turtle module
+speed('slowest')
+
+fd(100)
+lt(60)
+fd(100)
+lt(60)
+fd(100)
+lt(60)
+fd(100)
+lt(60)
+fd(100)
+lt(60)
+bk(100)
+rt(100)
+
+mainloop() # keep the window open until it is closed
